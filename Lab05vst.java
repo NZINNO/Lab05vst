21b9575473972a7d0c0a8c721b21e696ea62f10e
Zinno <4070806@vtsd.com>
2019-10-15 08:40:05 -0400

    Lab 05

--- Lab05vst.java
+++ Lab05vst.java
@@ -16,6 +16,19 @@ public class Lab05vst extends Applet
 		int y1 = 640;
 		int x2 = 990;
 		int y2 = 640;
-		g.drawRect(10,10,width,height);
+		g.drawRect(10,10,980,630);
+		for(int k=1; k<640; k+=10)
+		{
+			g.drawLine(x1,y1,x2,y2);
+			y2 -=10;
+			x1 +=15;
+		}
+		for(int k=1; k<640; k+=10)
+		{
+			g.drawLine(x2,y1,x1,y2);
+			y2 +=5;
+			x1 -=15;
+		}
 	}
+
 }
